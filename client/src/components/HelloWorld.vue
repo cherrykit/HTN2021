1e6be580c6e377151be2d547b6e9e158988f7ee7
Sophia Pietsch <cherrykit@outlook.com>
2021-09-17 23:37:27 -0400

    Added initial vue app.

---
+++ client/src/components/HelloWorld.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="container">
+    <p>{{ msg }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data() {
+    return {
+      msg: 'Hello!',
+    };
+  },
+};
+</script>
