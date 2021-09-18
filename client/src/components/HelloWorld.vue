a035c042a10bc316364ad3f553896bcb129b6622
denny <denlee973@gmail.com>
2021-09-18 02:13:45 -0400

    add basic text and change line endings

--- client/src/components/HelloWorld.vue
+++ client/src/components/HelloWorld.vue
@@ -1,6 +1,9 @@
 <template>
   <div class="container">
-    <p>{{ msg }}</p>
+    <h1>Title</h1>
+    <h3>Subtitle</h3>
+    <b-button type="submit" variant="primary">Upload your song!</b-button>
+    <b-form-file no-drop accept="mp3/*"></b-form-file>
   </div>
 </template>
 
